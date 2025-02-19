7c6683268bb042f80f9197f46cbb3aed74a5784e
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-19 12:51:12 +0530

    Update Transaction.java

--- src/main/java/org/example/Model/Transaction.java
+++ src/main/java/org/example/Model/Transaction.java
@@ -1,28 +1,5 @@
 package org.example.Model;
 
-import lombok.Getter;
-import lombok.Setter;
+public record Transaction (String transactionId,String transactionAmount,Account transactionFrom,Account transactionTo){
 
-import java.util.Date;
-
-@Getter @Setter public class Transaction {
-    private String transactionId;
-    private String accountNumber;
-    private double amount;
-    private String type;
-    private Date date;
-
-    public Transaction(String transactionId, String accountNumber, double amount, String type, Date date){
-        this.transactionId = transactionId;
-        this.accountNumber = accountNumber;
-        this.amount = amount;
-        this.type = type;
-        this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        return "Transaction{" + "transactionId='" + transactionId + "', accountNumber='" + accountNumber +
-                "', amount=" + amount + ", type='" + type + "', date=" + date + "}";
-    }
 }
