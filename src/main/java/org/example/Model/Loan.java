a12ed6fab32019d2d0447c4fb32cdfa884a25a0c
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-19 12:51:21 +0530

    Update Loan.java

--- src/main/java/org/example/Model/Loan.java
+++ src/main/java/org/example/Model/Loan.java
@@ -1,37 +1,15 @@
 package org.example.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
-
-@Getter @Setter public class Loan {
+@Getter @Setter @AllArgsConstructor public class Loan {
     private String loanId;
-    private String userId;
+    private String loanType;
     private double amount;
-    private double interestRate;
-    private int durationMonths;
-    private Date startDate;
-    private boolean isApproved;
-
-    public Loan(String loanId, String userId, double amount, double interestRate, int durationMonths, Date startDate, boolean isApproved){
-        this.loanId = loanId;
-        this.userId = userId;
-        this.amount = amount;
-        this.interestRate = interestRate;
-        this.durationMonths = durationMonths;
-        this.startDate = startDate;
-        this.isApproved = isApproved;
-    }
-
-    public double calculateTotalRepayment(){
-        return amount + (amount * (interestRate / 100) * (durationMonths / 12));
-    }
-
-    @Override
-    public String toString() {
-        return "Loan{" + "loanId='" + loanId + "', userId='" + userId + "', amount=" + amount +
-                ", interestRate=" + interestRate + ", durationMonths=" + durationMonths +
-                ", startDate=" + startDate + ", isApproved=" + isApproved + "}";
-    }
+    private double interest;
+    private User loanHolder;
+    private User approvedBy;
+    private boolean isDeleted;
 }
