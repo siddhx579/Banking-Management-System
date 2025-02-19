4b6adbe7ce8eb36b17aa90a75aabf01cb5568852
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-19 12:51:00 +0530

    Update Account.java

--- src/main/java/org/example/Model/Account.java
+++ src/main/java/org/example/Model/Account.java
@@ -1,38 +1,20 @@
 package org.example.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter class Account {
+@Entity @Getter @Setter @AllArgsConstructor public class Account {
+    @Id
+    private String accountId;
     private String accountNumber;
-    private String userId;
-    private String accountType;
-    private double balance;
 
-    public Account(String accountNumber, String userId, String accountType, double balance){
-        this.accountNumber = accountNumber;
-        this.userId = userId;
-        this.accountType = accountType;
-        this.balance = balance;
-    }
-
-    public void deposit(double amount){
-        if(amount > 0){
-            this.balance += amount;
-        }
-    }
-
-    public boolean withdraw(double amount){
-        if(amount > 0 && amount <= balance){
-            this.balance -= amount;
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" + "accountNumber='" + accountNumber + "', userId='" + userId +
-                "', balance=" + balance + ", accountType='" + accountType + "'}";
-    }
+    @OneToOne
+    private Customer accountHolder;
+    private String branchName;
+    private double amount;
+    private boolean isDeleted = false;
 }
