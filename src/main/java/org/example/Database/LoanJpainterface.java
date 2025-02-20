53b9b92aae862a47e544a4be372bef9f4b327937
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-21 00:39:06 +0530

    Update LoanJpainterface.java

--- src/main/java/org/example/Database/LoanJpainterface.java
+++ src/main/java/org/example/Database/LoanJpainterface.java
@@ -2,11 +2,13 @@ package org.example.Database;
 
 import org.example.Model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LoanJpainterface extends JpaRepository<Loan, String> {
-    List<Loan> findByLoanId(String loanId);
-    List<Loan> findByHolder(String loanHolder);
-    List<Loan> findByApprovedBy(String approvedBy);
+    Optional<Loan> findByLoanId(String loanId);
+    List<Loan> findByLoanType(@Param("loanType") String loanType);
+    List<Loan> findByLoanHolderId(@Param("userId") String userId);
 }
